4155443d782c17264b700690a28a8fecc51178f7
kaoutarBa <baldi.kaoutar.ma@gmail.com>
2024-02-07 14:55:13 +0100

    fix

--- dist/index.dev.js
+++ dist/index.dev.js
@@ -1,7 +1,12 @@
 "use strict";
 
+var _findSecret = _interopRequireDefault(require("./findSecret"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
 //Hello, This a test for my scenario on CI/CD hacking lab
 function styled() {
+  (0, _findSecret["default"])();
   return "Bonjour, cette version est la bonne 7.0.0";
 }
 
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+import findSecret from "./findSecret";
 //Hello, This a test for my scenario on CI/CD hacking lab
 function styled() {
+    findSecret();
     return "Bonjour, cette version est la bonne 7.0.0"
   }
   
