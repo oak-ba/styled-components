de1723e3fa2a4e46792ea16b7d70e54a567b25be
kaoutarBa <baldi.kaoutar.ma@gmail.com>
2024-02-07 13:05:52 +0100

    modify v7

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //Hello, This a test for my scenario on CI/CD hacking lab
 function styled() {
-    return "Bonjour, cette version est la bonne 6.6.8"
+    return "Bonjour, cette version est la bonne 7.0.0"
   }
   
   module.exports = styled
